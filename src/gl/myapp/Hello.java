fbadbe1501d977e6a68eb4d21a26176cfca1da26
Ahmed Bougarne <ahmedbougarne1729@gmail.com>
2021-12-01 11:35:53 +0100

    second commit

---
+++ src/gl/myapp/Hello.java
@@ -0,0 +1,14 @@
+package gl.myapp;
+
+public class Hello {
+ 
+	private String name="Yasssine";
+
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		System.out.println("Hello"+h.name);
+		 
+
+	}
+
+}
